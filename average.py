0aa3d9cf524ab9066a09ce1e777a0d99ab7b727c
ransom-iv <71794512+ransom-iv@users.noreply.github.com>
2020-09-30 17:59:37 -0400

    Add files via upload
    the "while"

--- average.py
+++ average.py
@@ -9,6 +9,8 @@ average = 0.0
 numberOfScores = int(input("Please enter the number of scores you want to average: "))
 
 #Add a loop to make this code repeat until scoreCount = numberOfScores
+
+while x <= 10
 score = int(input("Please enter a score: "))
 total = total + score
 scoreCount = scoreCount + 1
